8c6591c7fb0766e1770fbd02826e571dbc2c3b5c
Deepak Sardana <er.deepaksardana@gmail.com>
2017-12-21 13:55:34 +0530

    dunamic component

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -29,16 +29,6 @@ export class DynamicComponent implements OnInit, OnDestroy {
             return;
         }
 
-
-
-
-        // let componentFactory = this.resolver.resolveComponentFactory(data.component);
-        // let viewContainerRef = this.componentHost.viewContainerRef;
-        // let componentRef = viewContainerRef.createComponent(componentFactory);
-        // (componentRef.instance).data = data.inputs;
-
-
-
         // Inputs need to be in the following format to be resolved properly
         const inputProviders = Object.keys(data.inputs).map((inputName) => {
             return {
