5f2ed9edadea49209c5e26afcc060cb2c9518ec7
Deepak Sardana <er.deepaksardana@gmail.com>
2017-12-21 18:03:26 +0530

    http service request

--- src/app/core/_services/http.service.ts
+++ src/app/core/_services/http.service.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
-import { catchError, finalize, map} from 'rxjs/operators';
+import { catchError, finalize, map } from 'rxjs/operators';
 export class HttpService implements HttpInterceptor {
   requestCalls: number;
   constructor() {
@@ -17,45 +17,35 @@ export class HttpService implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.showLoader();
+    this.requestCalls++;
+    if (this.requestCalls === 1) {
+      // Show Loading
+      this.showLoader();
+    }
+    // append authentications if any;
+
     return next.handle(request).pipe(map(event => {
-      console.log('2');
       return event;
-      
     }),
-    catchError(error => {
-      return Observable.throw(error)
-    }),
-    finalize(() => {
-      console.log('1');
-    })
-
-  )
-    
-    // .do((event: HttpEvent<any>) => {
-    //   this.hideLoader();  
-    //   console.log('1');
-    //   if (event instanceof HttpResponse) {
-    //     // do stuff with response if you want
-    //   }
-    // }, (err: any) => {
-    //   console.log('2');
-    //     this.hideLoader();
-    //   if (err instanceof HttpErrorResponse) {
-    //     if (err.status === 401) {
-    //       // redirect to the login route
-    //       // or show a modal
-    //     }
-    //   }
-    // });
+      catchError(error => {
+        // redirect if required anything
+        return Observable.throw(error);
+      }),
+      finalize(() => {
+        this.requestCalls--;
+        if (this.requestCalls === 0) {
+          this.hideLoader();
+        }
+      })
+    );
   }
-    private showLoader(): void {
-        // this.loaderService.show();
-        console.log('show loader');
-    }
+  private showLoader(): void {
+    // this.loaderService.show();
+    console.log('show loader');
+  }
 
-    private hideLoader(): void {
-        // this.loaderService.hide();
-        // console.log('hide loader');
-    }
+  private hideLoader(): void {
+    // this.loaderService.hide();
+    console.log('hide loader');
+  }
 }
